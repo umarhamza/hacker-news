d0c7a0abe339af524b11aa91cbd5b6b389e222e6
umar-hamza <umar.hamza@finalsite.com>
2021-04-27 19:55:53 +0100

    updating dirname

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
-import { Layout } from './components/layouts/Layout';
-import { Stories } from './components/story/Stories';
+import { Layout } from './components/layout/Layout';
+import { Stories } from './components/stories/Stories';
 
 export const App = () => {
   const theme = createMuiTheme();
