376ed723ed9fdf0975b1346bfa29ec7cb475f54a
umar-hamza <umar.hamza@finalsite.com>
2021-04-27 19:29:55 +0100

    making final changes

--- src/components/Story/Stories.js
+++ src/components/Story/Stories.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Story } from './Story';
-import { getStoriesById } from '../../services/api';
+import { getStories } from '../../services/api';
 
 export const Stories = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
-    getStoriesById().then((ids) => setStoryIds(ids));
+    getStories().then((ids) => setStoryIds(ids));
   }, []);
-
-  // return <Story storyId='26957215' />;
 
   return storyIds
     .slice(0, 10)
--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ export const baseUrl = 'https://hacker-news.firebaseio.com/v0';
 export const newStoriesUrl = `${baseUrl}/newstories.json`;
 export const itemUrl = `${baseUrl}/item/`;
 
-export const getStoriesById = async () => {
+export const getStories = async () => {
   try {
     const res = await axios.get(newStoriesUrl).then(({ data }) => data);
     return res;
